0976a1280ad405c526a1e2639e516b2541f4b683
Damar Galih <anahariza45.com>
2024-09-03 08:19:06 +0700

    New

--- lib/widgets/input_widget.dart
+++ lib/widgets/input_widget.dart
@@ -12,13 +12,13 @@ class InputWidget extends StatelessWidget {
         // Calculate the text and icon size based on screen width
         double textSize = screenWidth * 0.05; // Adjust this value as needed
         double subTextSize = screenWidth * 0.03; // Adjust this value as needed
-        double iconSize = screenWidth * 0.4; // Adjust this value as needed
+        double iconSize = screenWidth * 0.3; // Adjust this value as needed
 
         return Container(
           width: screenWidth * 0.9, // Make the container width responsive
           padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 25),
           decoration: ShapeDecoration(
-            color: Color(0xFF3C83F5),
+            color: Color.fromARGB(255, 52, 177, 249),
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(12),
             ),
@@ -42,7 +42,8 @@ class InputWidget extends StatelessWidget {
                     SizedBox(
                       width: screenWidth * 0.5,
                       child: Text(
-                        'Fitur Canggih dan Terbaru',
+                        'Fitur Terbaru',
+                       
                         style: TextStyle(
                           color: Colors.white,
                           fontSize: textSize, // Use dynamic text size
@@ -55,7 +56,7 @@ class InputWidget extends StatelessWidget {
                     SizedBox(
                       width: screenWidth * 0.6,
                       child: Text(
-                        'Menawarkan teknologi terbaru dan terkini, proses katalogisasi Perpustakaan menjadi lebih cepat dan akurat.',
+                        'Otobook dirancang untuk pustakawan dengan Teknologi terbaru dan canggih untuk performa katalogisasi perpustakaan yang lebih baik.',
                         style: TextStyle(
                           color: Colors.white.withOpacity(0.96),
                           fontSize: subTextSize, // Use dynamic subtext size
@@ -71,8 +72,8 @@ class InputWidget extends StatelessWidget {
                 right: 0,
                 top: 0,
                 child: Container(
-                  width: iconSize * 0.8, // Adjust icon size
-                  height: iconSize * 0.8,
+                  width: iconSize, // Adjust icon size
+                  height: iconSize,
                   child: SvgPicture.asset(
                     'assets/icon/Kuning.svg',
                     fit: BoxFit.contain,
