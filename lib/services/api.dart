02746ab56718b1d68f2280306f6ca56dac209211
HCMPublic <HCMPublic@D-MacBookPro-2.local>
2024-10-29 15:31:26 +0700

    fix

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -78,8 +78,8 @@ class GetData {
   // Delete klasifikasi
   String get deleteKlasifikasiUrl => '${_apiUrl}deleteKlasifikasi';
 
-  static const String baseUrl = 'http://192.168.1.4:5000/api/getBuku';
-  static const String sinopsisUrl = 'http://192.168.1.4:5000/api/getSinopsis';
+  static const String baseUrl = 'http://103.106.72.182:8770/api//getBuku';
+  static const String sinopsisUrl = 'http://103.106.72.182:8770/api//getSinopsis';
 
   static Future<List<masterBook>> getBooks() async {
     try {
