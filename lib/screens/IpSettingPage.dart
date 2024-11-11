397c8a7e8eda3ef77687e4f37f7a646433264544
Mohammad Noer Dafiq <dafiqmohamad@gmail.com>
2024-11-11 22:29:08 +0700

    done

--- lib/screens/IpSettingPage.dart
+++ lib/screens/IpSettingPage.dart
@@ -14,19 +14,7 @@ class IpSettingsPage extends StatefulWidget {
 }
 
 class _IpSettingsPageState extends State<IpSettingsPage> {
-  // List<String> ipList = ['192.168.9.62', '192.168.1.2', '192.168.1.3'];
-
-  // String? selectedIp;
-  // String username = '';
-  // String password = '';
-  // Fungsi untuk menjalankan automasi
   void _runAutomation() async {
-    // if (selectedIp == null && username.isEmpty && password.isEmpty) {
-    //   ScaffoldMessenger.of(context).showSnackBar(
-    //     SnackBar(content: Text('Tidak ada IP yang dipilih atau username dan password kosong.')),
-    //   );
-    //   return;
-    // }
     Uri url = Uri.parse(
         '${GetData().runAutomationUrl}/${widget.bookId}'); // Mengambil bookId dari widget
     try {
@@ -46,6 +34,7 @@ class _IpSettingsPageState extends State<IpSettingsPage> {
       if (response.statusCode == 200) {
         final responseData = json.decode(response.body);
         print(responseData['message']);
+
         // Simpan pesan ke SharedPreferences
         SharedPreferences prefs = await SharedPreferences.getInstance();
         await prefs.setString(
@@ -71,82 +60,16 @@ class _IpSettingsPageState extends State<IpSettingsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Pengaturan IP"),
+        title: Text("Trigger Automasi"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: <Widget>[
-            // Input untuk username
-            // TextFormField(
-            //   decoration: InputDecoration(
-            //     labelText: 'Username',
-            //     border: OutlineInputBorder(),
-            //   ),
-            //   onChanged: (value) {
-            //     setState(() {
-            //       username = value;
-            //     });
-            //   },
-            // ),
-            // SizedBox(height: 20),
-            // // Input untuk password
-            // TextFormField(
-            //   decoration: InputDecoration(
-            //     labelText: 'Password',
-            //     border: OutlineInputBorder(),
-            //   ),
-            //   obscureText: true, // Menyembunyikan input password
-            //   onChanged: (value) {
-            //     setState(() {
-            //       password = value;
-            //     });
-            //   },
-            // ),
-            // SizedBox(height: 20.0),
-            // Text('Pilih Alamat IP:'),
-            // // Membuat daftar checkbox untuk setiap IP
-            // Column(
-            //   children: ipList.map((ip) {
-            //     return RadioListTile<String>(
-            //       title: Text(ip),
-            //       value: ip,
-            //       groupValue: selectedIp,
-            //       onChanged: (String? value) {
-            //         setState(() {
-            //           selectedIp = value;
-            //         });
-            //       },
-            //     );
-            //   }).toList(),
-            // ),
-            // SizedBox(height: 20),
             SizedBox(height: 20),
-
             ElevatedButton(
               onPressed: () {
-                // if (username.isNotEmpty &&
-                //     password.isNotEmpty &&
-                //     selectedIp != null) 
-                {
-                  _runAutomation(); // Jalankan automasi setelah IP dipilih
-                // } else {
-                //   ScaffoldMessenger.of(context).showSnackBar(
-                //     SnackBar(
-                //         content: Text(
-                //             'Harap isi username, password, dan pilih IP terlebih dahulu.')),
-                //   );
-                }
-                //     selectedIp != null)
-                // {
                 _runAutomation(); // Jalankan automasi setelah IP dipilih
-                //   } else {
-                //     ScaffoldMessenger.of(context).showSnackBar(
-                //       SnackBar(
-                //           content: Text(
-                //               'Harap isi username, password, dan pilih IP terlebih dahulu.')),
-                //     );
-                //   }
               },
               child: Text(
                 'Simpan IP dan Jalankan Automasi untuk Book ID: ${widget.bookId}',
--- lib/services/api.dart
+++ lib/services/api.dart
@@ -4,13 +4,13 @@ import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class GetData {
-  final String _apiUrl = 'http://103.106.72.182:8770/api/';
+  final String _apiUrl = 'http://192.168.9.214:5000/api/';
   Future<String?> _userId() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     return prefs.getString('id');
   }
 
-  String get Url => 'http://103.106.72.182:8770';
+  String get Url => 'http://192.168.9.214:5000';
 
   // login user
   String get loginUrl => '${_apiUrl}login';
@@ -78,9 +78,8 @@ class GetData {
   // Delete klasifikasi
   String get deleteKlasifikasiUrl => '${_apiUrl}deleteKlasifikasi';
 
-  static const String baseUrl = 'http://103.106.72.182:8770/api/getBuku';
-  static const String sinopsisUrl =
-      'http://103.106.72.182:8770/api/getSinopsis';
+  static const String baseUrl = 'http://192.168.9.214:5000/api/getBuku';
+  static const String sinopsisUrl = 'http://192.168.9.214:5000/api/getSinopsis';
 
   static Future<List<masterBook>> getBooks() async {
     try {
