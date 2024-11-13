7f1d7f99f4f2f772ddc210b67374ab32cc75d717
D4marp <damargalihajipradana@it.student.pens.ac.id>
2024-11-13 20:13:20 +0700

    new

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=E:\Flutter\flutter_windows_3.19.0-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\KULIAH\OTOBOOK\Mobile--App-OTOBOOK-"
+export "FLUTTER_ROOT=/Users/HCMPublic/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/HCMPublic/app/Mobile--App-OTOBOOK-"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
